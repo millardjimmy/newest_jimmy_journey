ff47acfe8cce3cee7aeb324beddecdd577d84732
millardjimmy <millardjimmy@gmail.com>
2020-05-26 13:05:06 -0600

    first commit

---
+++ lib/jimmy_journey.rb
@@ -0,0 +1,6 @@
+require "jimmy_journey/version"
+
+module JimmyJourney
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jimmy_journey/version.rb
@@ -0,0 +1,3 @@
+module JimmyJourney
+  VERSION = "0.1.0"
+end
